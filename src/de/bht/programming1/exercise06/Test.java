143c2ae1fa4f77e610ea48912ab01065607ce77e
Jan Rabe <jan.rabe@wooga.net>
2014-09-04 18:11:01 +0200

    task 6

---
+++ src/de/bht/programming1/exercise06/Test.java
@@ -0,0 +1,10 @@
+package de.bht.programming1.exercise06;
+
+import pr1.easy.painter.v03.Painter;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Painter.main(null);
+    }
+}
