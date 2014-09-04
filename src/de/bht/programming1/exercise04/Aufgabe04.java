95cd4637fe36974366ad8023d6e4d4b4c3573b0d
Jan Rabe <jan.rabe@wooga.net>
2014-09-04 16:05:44 +0200

    added task 4

---
+++ src/de/bht/programming1/exercise04/Aufgabe04.java
@@ -0,0 +1,221 @@
+package de.bht.programming1.exercise04;
+
+import lernhilfe.v2010ws01.Adresse;
+import lernhilfe.v2010ws01.Einwohner;
+import lernhilfe.v2010ws01.Person;
+import lernhilfe.v2010ws01.Personenliste;
+import pr1.qad.DirtyFileWriter;
+
+import java.io.File;
+import java.io.PrintWriter;
+
+public class Aufgabe04 {
+
+    public static final String FILEPATH = "assets/files/listen/";
+    public static final String FILENAME_EINFACHE_OBJEKTE = "einfache-objekte.txt";
+    public static final String FILENAME_ALL_BASIC_LISTS = "all-basic-lists.txt";
+    public static final String FILENAME_COMPLEX_LISTS = "complex-lists.txt";
+
+    public static final PrintWriter cout = new PrintWriter(System.out, true);
+
+    public static void main(String[] args) {
+
+        File f = new File(FILEPATH);
+        f.mkdirs();
+
+        aufgabeb1();
+        aufgabeb2();
+        aufgabeb3();
+        aufgabe4c();
+        aufgabe4e();
+    }
+
+    public static void aufgabeb1() {
+        Person person1 = new Person("Kurt", "Bangert", 1988);
+        Person person2 = new Person("Gerhard", "Ackermann", 1956);
+
+        Adresse adresse1 = new Adresse(99553, "Musterort", "Musterstraße", 1);
+        Adresse adresse2 = new Adresse(58821, "Musterdort", "Musterstraße", 2);
+
+        Einwohner einwohner1 = new Einwohner(person1, adresse1);
+        Einwohner einwohner2 = new Einwohner(person2, adresse2);
+
+        cout.println(person1);
+        cout.println(person2);
+        cout.println(adresse1);
+        cout.println(adresse2);
+        cout.println(einwohner1);
+        cout.println(einwohner2);
+    }
+
+    public static void aufgabeb2() {
+        Person person1 = new Person("Kurt", "Bangert", 1988);
+        Adresse adresse1 = new Adresse(99553, "Musterort", "Musterstraße", 1);
+        Einwohner einwohner1 = new Einwohner(person1, adresse1);
+
+        // Ein Einwohnerobjekt kann auch als Personobjekt verwendet werden.
+        // stimmt, weil Einwohner extends Person
+        Einwohner einwohner = new Einwohner(einwohner1, adresse1);
+
+        // Ein Adresseobjekt kann auch als Personobjekt verwendet werden.
+        // geht nicht, weil Adresse-Klasse, nicht von Personenklasse erbt
+        // einwohner = new Einwohner(addresse1, adresse1);
+    }
+
+    public static void aufgabeb3() {
+
+        PrintWriter fout = new PrintWriter(new DirtyFileWriter(FILEPATH + FILENAME_EINFACHE_OBJEKTE), true);
+
+        fout.println("vor Hochzeit");
+
+        // person 1
+        Person person1 = new Person("Kurt", "Bangert", 1988);
+        Adresse adresse1 = new Adresse(99553, "Berlin", "Berliner Straße", 20);
+        Einwohner einwohner1 = new Einwohner(person1, adresse1);
+        fout.println(einwohner1);
+
+        // person 2
+        Person person2 = new Person("Anna", "Schmidt", 87);
+        Adresse adresse2 = new Adresse(58821, "Nürnberg", "Nürnberger Straße", 2);
+        Einwohner einwohner2 = new Einwohner(person2, adresse2);
+        fout.println(einwohner2);
+
+        // hochzeit
+        einwohner1.heiraten(einwohner2);
+
+        fout.println("\nnach Hochzeit");
+        fout.println(einwohner1);
+        fout.println(einwohner2);
+
+        // umzug
+        einwohner2.umziehenNach(einwohner1.getAdresse());
+
+        fout.println("\nnach Hochzeit");
+        fout.println(einwohner1);
+        fout.println(einwohner2);
+
+        // release file
+        fout.close();
+    }
+
+    public static void aufgabe4c() {
+
+        PrintWriter fout = new PrintWriter(new DirtyFileWriter(FILEPATH + FILENAME_ALL_BASIC_LISTS), true);
+
+        // die Liste aller 10 Sportfreunde
+        Personenliste sportFreunde = new Personenliste();
+        sportFreunde.add(new Person("Kurt", "Bangert", 1988));
+        sportFreunde.add(new Person("Peter", "Bartsch", 1954)); // auch verwandt
+        sportFreunde.add(new Person("Reinhard", "Barrabas", 1989)); // auch kommilitone
+        sportFreunde.add(new Person("Dirk-Rainer", "Blomeyer", 1966)); // auch komilitone
+        // etc.
+
+        // die Liste aller 10 Kommilitionen
+        Personenliste komilitonen = new Personenliste();
+        komilitonen.add(new Person("Gerhard", "Ackermann", 1956)); // auch verwandt
+        komilitonen.add(new Person("Martin", "Behne", 1967));
+        komilitonen.add(new Person("Dirk-Rainer", "Blomeyer", 1966)); // auch sportsfreund
+        komilitonen.add(new Person("Reinhard", "Barrabas", 1989)); // auch sportsfreund
+        // etc.
+
+        // die Liste aller 10 Verwandten.
+        Personenliste verwandte = new Personenliste();
+        verwandte.add(new Person("Peter", "Arnke", 1977));
+        verwandte.add(new Person("Jürgen", "Berger", 1988));
+        verwandte.add(new Person("Stephan", "Braunfels", 1958));
+        verwandte.add(new Person("Peter", "Bartsch", 1954)); // auch sportsfreund
+        verwandte.add(new Person("Gerhard", "Ackermann", 1956));  // auch kommilitone
+        // etc.
+
+        // write sportsfreunde to file
+        for (int i = 0; i < sportFreunde.size(); ++i) {
+            fout.println(sportFreunde.get(i));
+        }
+
+        fout.println();
+
+        // write kommilitonen to file
+        for (Person p : komilitonen) {
+            fout.println(p);
+        }
+
+        fout.println();
+
+        // write verwandte to file
+        for (Person p : verwandte) {
+            fout.println(p);
+        }
+
+        // release file
+        fout.close();
+    }
+
+    public static void aufgabe4e() {
+        PrintWriter fout = new PrintWriter(new DirtyFileWriter(FILEPATH + FILENAME_COMPLEX_LISTS), true);
+
+        // die Liste aller 10 Sportfreunde
+        Personenliste sportFreunde = new Personenliste();
+        sportFreunde.add(new Person("Kurt", "Bangert", 1988));
+        sportFreunde.add(new Person("Peter", "Bartsch", 1954)); // auch verwandt
+        sportFreunde.add(new Person("Reinhard", "Barrabas", 1989)); // auch kommilitone
+        sportFreunde.add(new Person("Dirk-Rainer", "Blomeyer", 1966)); // auch komilitone
+        // etc.
+
+        // die Liste aller 10 Kommilitionen
+        Personenliste komilitonen = new Personenliste();
+        komilitonen.add(new Person("Gerhard", "Ackermann", 1956)); // auch verwandt
+        komilitonen.add(new Person("Martin", "Behne", 1967));
+        komilitonen.add(new Person("Dirk-Rainer", "Blomeyer", 1966)); // auch sportsfreund
+        komilitonen.add(new Person("Reinhard", "Barrabas", 1989)); // auch sportsfreund
+        // etc.
+
+        // die Liste aller 10 Verwandten.
+        Personenliste verwandte = new Personenliste();
+        verwandte.add(new Person("Peter", "Arnke", 1977));
+        verwandte.add(new Person("Jürgen", "Berger", 1988));
+        verwandte.add(new Person("Stephan", "Braunfels", 1958));
+        verwandte.add(new Person("Peter", "Bartsch", 1954)); // auch sportsfreund
+        verwandte.add(new Person("Gerhard", "Ackermann", 1956));  // auch kommilitone
+        // etc.
+
+        // aller Sportfreunde, die auch Kommilitonen sind
+        Personenliste sportsFreundeUndKommilitonen = new Personenliste();
+        sportsFreundeUndKommilitonen.addAll(sportFreunde);
+        // Menge = Sportsfreunde und Kommilitonen
+        sportsFreundeUndKommilitonen.retainAll(komilitonen);
+
+        // aller Verwandten, die nicht Sportfreunde sind
+        Personenliste verwandteUndKeineSportsfreunde = new Personenliste();
+        verwandteUndKeineSportsfreunde.addAll(verwandte);
+        // Menge = Verwandte und nicht Sportfreunde
+        verwandteUndKeineSportsfreunde.removeAll(sportFreunde);
+
+        // aller Verwandten und Kommilitonen
+        Personenliste verwandteUndKommilitonen = new Personenliste();
+        verwandteUndKommilitonen.addAll(verwandte);
+        // doppelte, da Person, hash and equals nicht ordentlich überschreibt!
+        verwandteUndKommilitonen.addAll(komilitonen);
+
+        // write sportsFreundeUndKommilitonen to file
+        for (Person p : sportsFreundeUndKommilitonen) {
+            fout.println(p);
+        }
+
+        fout.println();
+
+        // write verwandteUndKeineSportsfreunde to file
+        for (Person p : verwandteUndKeineSportsfreunde) {
+            fout.println(p);
+        }
+
+        fout.println();
+
+        // write verwandteUndKommilitonen to file
+        for (Person p : verwandteUndKommilitonen) {
+            fout.println(p);
+        }
+
+        // release file
+        fout.close();
+    }
+}
---
+++ src/de/bht/programming1/exercise04/Personen.java
@@ -0,0 +1,163 @@
+package de.bht.programming1.exercise04;
+
+import lernhilfe.v2010ws01.Adresse;
+import lernhilfe.v2010ws01.Einwohner;
+import lernhilfe.v2010ws01.Person;
+import lernhilfe.v2010ws01.Personenliste;
+import pr1.qad.DirtyFileWriter;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Personen {
+
+    public static void main(String[] args) {
+        aufgabeB();
+        aufgabeC();
+        // aufgabeE();
+    }
+
+    public static void aufgabeB() {
+        PrintWriter out = new PrintWriter(System.out, true);
+
+        // Aufgabe 4.b.1)
+        Person max = new Person("Max", "Mustermann", 1962);
+        out.println(max);
+        Person erika = new Person("Erika", "Musterfrau", 1969);
+        out.println(erika);
+        Adresse anschriftM = new Adresse(12345, "Musterstadt", "Rosenweg", 11);
+        out.println(anschriftM);
+        Adresse anschriftE = new Adresse(54321, "Neustadt", "Hauptstra�e", 20);
+        out.println(anschriftE);
+        Einwohner buergerM = new Einwohner(max, anschriftM);
+        out.println(buergerM);
+        Einwohner buergerE = new Einwohner(erika, anschriftE);
+        out.println(buergerE);
+        out.println("*******************************************************");
+
+        // Aufgabe 4.b.2)
+        Person x = new Einwohner(max, null);
+        out.println(x);
+        // Einwohner ist Teilmenge von Person -> Aussage ist wahr
+        // Person y = new Adresse(12345, "Musterstadt", "Rosenweg", 11);
+        // Adresse ist keine Person -> Aussage falsch
+        out.println("*******************************************************");
+
+        // Aufgabe 4.b.3)
+        PrintWriter fout = new PrintWriter(new DirtyFileWriter("./listen/einfache-objekte.txt"));
+        out.println("Erika heiratet Max");
+        max.heiraten(erika);
+        out.println(max);
+        out.println(erika);
+        fout.println(max);
+        fout.println(erika);
+
+        out.println("Max zieht um zu Erika");
+        buergerM.umziehenNach(anschriftE);
+        out.println(buergerM);
+        fout.println(buergerM);
+        fout.flush();
+        fout.close();
+    }
+
+    public static void aufgabeC() {
+//			Aufgabe 4.c)
+//			Sportsfreunde
+        Person peter = new Person("Peter", "Müller", 1978);
+        Person thomas = new Person("Thomas", "Schmidt", 1981);
+        Person sabine = new Person("Sabine", "Maier", 1977);
+        Person moritz = new Person("Moritz", "Schuhmann", 1984);
+        Person kerstin = new Person("Kerstin", "Becker", 1979);
+        Person paul = new Person("Paul", "Walter", 1987);
+        Person martin = new Person("Martin", "Sommer", 1986);
+        Person david = new Person("David", "Schulz", 1982);
+        Person sandra = new Person("Sandra", "Fritsche", 1985);
+        Person nina = new Person("Nina", "Baumann", 1983);
+
+//			Kommilitonen
+        Person lucas = new Person("Lucas", "Winter", 1984);
+        Person daniel = new Person("Daniel", "Stain", 1981);
+        Person dirk = new Person("Dirk", "Burkhard", 1977);
+        Person laura = new Person("Laura", "Raimann", 1989);
+        Person susanne = new Person("Susanne", "Hofmeister", 1980);
+        Person fabian = new Person("Fabian", "Krämer", 1983);
+        Person tim = new Person("Tim", "Kaufmann", 1985);
+        Person kathi = new Person("Kathi", "Gerber", 1987);
+        Person anika = new Person("Anika", "Rose", 1988);
+        Person florian = new Person("Florian", "Mann", 1979);
+
+//			Verwandte
+        Person timo = new Person("Timo", "Schuster", 1982);
+        Person sven = new Person("Sven", "Svenson", 1977);
+        Person anna = new Person("Anna", "Schulz", 1983);
+        Person birgit = new Person("Birgit", "Krämer", 1960);
+        Person marcus = new Person("Marcus", "Neumann", 1975);
+        Person andrea = new Person("Andrea", "Becker", 1984);
+        Person michael = new Person("Michael", "Rose", 1973);
+        Person kristina = new Person("Kristina", "Hausmann", 1981);
+        Person christian = new Person("Christian", "Kunz", 1991);
+        Person klaus = new Person("Klaus", "Krause", 1969);
+
+        Personenliste sportfreunde = new Personenliste();
+        sportfreunde.add(peter);
+        sportfreunde.add(thomas);
+        sportfreunde.add(sabine);
+        sportfreunde.add(moritz);
+        sportfreunde.add(kerstin);
+        sportfreunde.add(paul);
+        sportfreunde.add(martin);
+        sportfreunde.add(david);
+        sportfreunde.add(sandra);
+        sportfreunde.add(nina);
+        sportfreunde.add(andrea);
+        sportfreunde.add(sven);
+//			for(String s= 'peter'; s <= 'sven'; s++){
+//				PrintWriter fout1 = new PrintWriter(new DirtyFileWriter ("./listen/all-basic-lists.txt"));
+//				fout1.println(s);
+//			}
+
+        Personenliste kommilitonen = new Personenliste();
+        kommilitonen.add(lucas);
+        kommilitonen.add(daniel);
+        kommilitonen.add(dirk);
+        kommilitonen.add(laura);
+        kommilitonen.add(susanne);
+        kommilitonen.add(fabian);
+        kommilitonen.add(tim);
+        kommilitonen.add(kathi);
+        kommilitonen.add(anika);
+        kommilitonen.add(florian);
+        kommilitonen.add(moritz);
+        kommilitonen.add(sabine);
+        kommilitonen.add(paul);
+
+        Personenliste verwandte = new Personenliste();
+        verwandte.add(timo);
+        verwandte.add(sven);
+        verwandte.add(anna);
+        verwandte.add(birgit);
+        verwandte.add(marcus);
+        verwandte.add(andrea);
+        verwandte.add(michael);
+        verwandte.add(kristina);
+        verwandte.add(christian);
+        verwandte.add(klaus);
+        verwandte.add(kerstin);
+        verwandte.add(daniel);
+        verwandte.add(paul);
+    }
+
+    public static void aufgabeE(Scanner in) {
+//			alle Sportfreunde, die auch Kommilitonen sind
+//			Personenliste sportfreunde= new Personenliste();
+//			while(in.hasNext()){
+//				sportfreunde.retainAll(kommilitonen(in));
+//				for(){
+//					PrintWriter fout = new PrintWriter(new DirtyFileWriter ("./listen/all-coplex-lists.txt"));
+//					fout.println(s);
+//				}
+//			}
+    }
+
+
+}
