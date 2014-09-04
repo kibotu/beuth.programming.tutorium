459ee998b89d8de9c8a498a59a6afb236a953121
Jan Rabe <jan.rabe@wooga.net>
2014-09-04 13:37:56 +0200

    added libs

--- src/de/bht/programming1/exercis01/Main.java
+++
@@ -1,8 +0,0 @@
-package de.bht.programming1.exercis01;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/de/bht/programming1/exercise01/HelloJava.java
@@ -0,0 +1,8 @@
+package de.bht.programming1.exercise01;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
